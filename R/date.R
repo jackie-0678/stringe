fc540e457b274a24239dbb9465b1cffde04d1932
jackie-0678 <58518371+jackie-0678@users.noreply.github.com>
2019-12-04 11:29:42 -0500

    Update date.R
    removing function factory - functionality

--- R/date.R
+++ R/date.R
@@ -9,17 +9,15 @@
 #' clean_date()
 
 clean_date <- function(x,format = 'monthday') {
-  function(x){
-    require(stringr)
-    
-    input <-  c('%m/%d/%Y','%m-%d-%Y','%Y/%m/%d','%Y-%m-%d','%B %d %Y','%B %d, %Y','%B/%d/%Y','%B %d')
-    output <- c(monthday ='%B %d')
-  
-    x <- remove_ws(x)
-    x[is_valid(as.Date(x,tryFormats = input,optional=TRUE))] <- str_replace(format(as.Date(x,tryFormats = input,optional=TRUE),output[format]),'\\s[0]',' ')
+  require(stringr)
 
-    return(x)
-  }
+  input <-  c('%m/%d/%Y','%m-%d-%Y','%Y/%m/%d','%Y-%m-%d','%B %d %Y','%B %d, %Y','%B/%d/%Y','%B %d')
+  output <- c(monthday ='%B %d')
+
+  x <- remove_ws(x)
+  x[is_valid(as.Date(x,tryFormats = input,optional=TRUE))] <- str_replace(format(as.Date(x,tryFormats = input,optional=TRUE),output[format]),'\\s[0]',' ')
+
+  return(x)
 }
 
 #' valid date
@@ -34,29 +32,27 @@ clean_date <- function(x,format = 'monthday') {
 #' valid_date()
 
 valid_date <- function(x, format = 'all', allow = NULL, na_to_false = FALSE) {
-  function(x){
-    require(stringr)
-    
-    # logic
-    index <- 1:length(x)
-    index[index][!is_valid(x[index])] <- 0
-    
-    if(format == 'monthday'){
-      index[index][is.na(format(as.Date(x[index],'%B %d'),'%B %d'))] <- 0
-      index[index][str_detect(x[index], "\\s[0]")] <- 0
-    } else {
-      index[index][x[index] == clean_date()(x[index]) & is.na(format(as.Date(x[index],'%B %d'),'%B %d'))] <- 0
-    } 
-    
-    if(!is.null(allow)){index[which(x %in% allow)] <- which(x %in% allow)}
-    
-    # identify invalid values
-    if(na_to_false == FALSE){invalid <- which(!is_valid(x))}
-    
-    x <- return_values(index,invalid)
-    
-    return(x)
-  }
+  require(stringr)
+
+  # logic
+  index <- 1:length(x)
+  index[index][!is_valid(x[index])] <- 0
+
+  if(format == 'monthday'){
+    index[index][is.na(format(as.Date(x[index],'%B %d'),'%B %d'))] <- 0
+    index[index][str_detect(x[index], "\\s[0]")] <- 0
+  } else {
+    index[index][x[index] == clean_date()(x[index]) & is.na(format(as.Date(x[index],'%B %d'),'%B %d'))] <- 0
+  } 
+
+  if(!is.null(allow)){index[which(x %in% allow)] <- which(x %in% allow)}
+
+  # identify invalid values
+  if(na_to_false == FALSE){invalid <- which(!is_valid(x))}
+
+  x <- return_values(index,invalid)
+
+  return(x)
 }
 
 
