eca3f18faeeded0a75a583483cc719b527eff189
jandersonniche <janderson@niche.com>
2019-12-04 11:21:18 -0500

    adding stringe package

---
+++ R/numeric.R
@@ -0,0 +1,59 @@
+#' clean numeric
+#'
+#' Clean numeric data
+#' @param x
+#' @param decimals
+#' @keywords numeric
+#' @export
+#' @examples
+#' clean_numeric()
+
+clean_numeric <- function(x, decimals = 2) {
+  function(x){
+    require(stringr)
+    
+    x <- remove_ws(x)
+    x <- str_replace_all(x,'[^\\d\\.]','')
+    x[is_valid(suppressWarnings(as.numeric(x)))] <- formatC(as.numeric(x[is_valid(suppressWarnings(as.numeric(x)))]), digits = decimals, format = 'f')
+    
+    return(x)
+  }
+}
+
+
+#' valid numeric
+#'
+#' Identify numeric data that does not meet Niche's expectations
+#' @param x
+#' @param decimals
+#' @param minn
+#' @param maxx
+#' @param na_to_false
+#' @keywords numeric
+#' @export
+#' @examples
+#' valid_numeric()
+
+valid_numeric <- function(x, minn = 0, maxx = .Machine$integer.max, decimals = 2, na_to_false = FALSE) {
+  function(x){
+    require(stringr)
+  
+    # logic
+    index <- 1:length(x)
+    index[index][!is_valid(x[index])] <- 0
+    
+    ifelse(decimals == 0 | is.null(decimals), dot <- "", dot<-"\\.")
+    
+    index[index][str_detect(x[index],paste0("[^\\d",dot,"]"))] <- 0
+    index[index][!str_detect(x[index],paste0("(\\d",dot,"\\d{",decimals,"})$"))] <- 0
+    index[index][floor(as.numeric(x[index])) > .Machine$integer.max] <- 0
+    index[index][as.numeric(x[index]) < minn | as.numeric(x[index]) > maxx] <- 0
+    
+    # identify invalid values
+    if(na_to_false == FALSE){invalid = which(!is_valid(x))}
+    
+    x <- return_values(index,invalid)
+    
+    return(x)
+  }
+}
---
+++ R/string.R
@@ -0,0 +1,55 @@
+#' clean string
+#'
+#' Clean string data
+#' @param x
+#' @param set_regex
+#' @param set_case valid input can be 'title','lower','upper'
+#' @param set_recode input should be a named vector. ex: "remove char" = "replace char"
+#' @param FUN an additionanl function call
+#' @keywords string
+#' @export
+#' @examples
+#' clean_string()
+
+clean_string <- function(x, set_case = NULL, set_recode = NULL, FUN = NULL, ...){
+  function(x){
+    require(stringr)
+    
+    if(!is.null(set_recode)){x <- str_replace_all(x, set_recode)}
+    x <- remove_ws(x)
+    if(!is.null(set_case)){x <- text_case(x,set_case)}
+    if(!is.null(FUN)){x <- FUN(x, ...)}
+    
+    return(x)
+  }
+}
+
+#' valid string
+#'
+#' Identify string data that does not meet Niche's expectations 
+#' @param x
+#' @param distinct
+#' @param na_to_false
+#' @keywords distinct string
+#' @export
+#' @examples
+#' valid_string()
+
+valid_string <- function(x, distinct_vals = NULL, set_regex = NULL, na_to_false = FALSE){
+  function(x){
+    require(stringr)
+    
+    index <- 1:length(x)
+    index[index][!is_valid(x[index])] <- 0
+    
+    if(!is.null(set_regex)){index[index][!str_detect(x[index], paste0("(",set_regex,")"))] <- 0}
+    if(!is.null(distinct_vals)){index[index][!(x[index] %in% distinct_vals)] <- 0}
+    
+    # identify invalid values
+    if(na_to_false == FALSE){invalid <- which(!is_valid(x))}
+    
+    x <- return_values(index,invalid)
+    
+    return(x)
+  }
+}
