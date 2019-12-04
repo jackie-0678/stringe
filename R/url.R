b9872fb344aa3f0336262b11c8898c2dfe145996
jackie-0678 <58518371+jackie-0678@users.noreply.github.com>
2019-12-04 11:31:46 -0500

    Update url.R
    removing function factory - functionality

--- R/url.R
+++ R/url.R
@@ -9,21 +9,19 @@
 #' valid_url()
 
 valid_url <- function(x, na_to_false = FALSE) {
-  function(x){
-    require(stringr)
-    
-    index <- 1:length(x)
-    index[index][!is_valid(x[index])] <- 0
-    index[index][!(substr(x[index],1,4) == "http")] <- 0
-    index[index][str_detect(x[index],"//$")] <- 0
-    index[index][!vapply(x[index],valid_url_status,logical(1))] <- 0
-    
-    # identify invalid values
-    if(na_to_false == FALSE){invalid <- which(!is_valid(x))}
-    
-    x <- return_values(index,invalid)
-    
-    return(x)
-  }
+  require(stringr)
+
+  index <- 1:length(x)
+  index[index][!is_valid(x[index])] <- 0
+  index[index][!(substr(x[index],1,4) == "http")] <- 0
+  index[index][str_detect(x[index],"//$")] <- 0
+  index[index][!vapply(x[index],valid_url_status,logical(1))] <- 0
+
+  # identify invalid values
+  if(na_to_false == FALSE){invalid <- which(!is_valid(x))}
+
+  x <- return_values(index,invalid)
+
+  return(x)
 }
 
