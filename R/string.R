a8a599a3275ce17cac884bfe2347574853c7317d
jackie-0678 <58518371+jackie-0678@users.noreply.github.com>
2019-12-04 11:31:18 -0500

    Update string.R
    removing function factory - functionality

--- R/string.R
+++ R/string.R
@@ -12,16 +12,14 @@
 #' clean_string()
 
 clean_string <- function(x, set_case = NULL, set_recode = NULL, FUN = NULL, ...){
-  function(x){
-    require(stringr)
-    
-    if(!is.null(set_recode)){x <- str_replace_all(x, set_recode)}
-    x <- remove_ws(x)
-    if(!is.null(set_case)){x <- text_case(x,set_case)}
-    if(!is.null(FUN)){x <- FUN(x, ...)}
-    
-    return(x)
-  }
+  require(stringr)
+
+  if(!is.null(set_recode)){x <- str_replace_all(x, set_recode)}
+  x <- remove_ws(x)
+  if(!is.null(set_case)){x <- text_case(x,set_case)}
+  if(!is.null(FUN)){x <- FUN(x, ...)}
+
+  return(x)
 }
 
 #' valid string
@@ -36,20 +34,18 @@ clean_string <- function(x, set_case = NULL, set_recode = NULL, FUN = NULL, ...)
 #' valid_string()
 
 valid_string <- function(x, distinct_vals = NULL, set_regex = NULL, na_to_false = FALSE){
-  function(x){
-    require(stringr)
-    
-    index <- 1:length(x)
-    index[index][!is_valid(x[index])] <- 0
-    
-    if(!is.null(set_regex)){index[index][!str_detect(x[index], paste0("(",set_regex,")"))] <- 0}
-    if(!is.null(distinct_vals)){index[index][!(x[index] %in% distinct_vals)] <- 0}
-    
-    # identify invalid values
-    if(na_to_false == FALSE){invalid <- which(!is_valid(x))}
-    
-    x <- return_values(index,invalid)
-    
-    return(x)
-  }
+  require(stringr)
+
+  index <- 1:length(x)
+  index[index][!is_valid(x[index])] <- 0
+
+  if(!is.null(set_regex)){index[index][!str_detect(x[index], paste0("(",set_regex,")"))] <- 0}
+  if(!is.null(distinct_vals)){index[index][!(x[index] %in% distinct_vals)] <- 0}
+
+  # identify invalid values
+  if(na_to_false == FALSE){invalid <- which(!is_valid(x))}
+
+  x <- return_values(index,invalid)
+
+  return(x)
 }
